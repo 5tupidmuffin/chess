1b40f10c16306d5d4adfa45e41232aaf47c568ef
5tupidmuffin <rameshwark9500@gmail.com>
2022-03-21 18:56:47 +0530

    changed representation

--- scripts/debugTools/fenString.js
+++ scripts/debugTools/fenString.js
@@ -1,29 +1,19 @@
 export const fenToBoard = (fenString) => {
-  const pieceTable = {
-    // white pieces
-    K: 20,
-    Q: 29,
-    B: 24,
-    N: 23,
-    R: 25,
-    P: 21,
-    // black pieces
-    k: 10,
-    q: 19,
-    b: 14,
-    n: 13,
-    r: 15,
-    p: 11,
-  };
-
   let stringCursor = 0,
     boardCursor = 0,
-    board = [];
+    board = new Array(64);
 
   while (stringCursor < fenString.length && boardCursor < 64) {
     let currentLetter = fenString[stringCursor];
     if (isNaN(Number(currentLetter)) && currentLetter !== "/") {
-      board[boardCursor] = pieceTable[currentLetter];
+      let color =
+        fenString[stringCursor].toUpperCase() === fenString[stringCursor]
+          ? "w"
+          : "b";
+      board[boardCursor] = {
+        color,
+        piece: fenString[stringCursor].toLowerCase(),
+      };
       stringCursor++;
       boardCursor++;
       continue;
@@ -31,7 +21,7 @@ export const fenToBoard = (fenString) => {
     if (!isNaN(Number(currentLetter))) {
       stringCursor++;
       for (let i = 0; i < currentLetter; i++) {
-        board[boardCursor] = 0;
+        board[boardCursor] = null;
         boardCursor++;
       }
       continue;
