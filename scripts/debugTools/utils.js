daf0e46ad8a376e8720151897faa7475a63fd365
5tupidmuffin <rameshwark9500@gmail.com>
2022-04-03 00:19:34 +0530

    added style for debug labels

--- scripts/debugTools/utils.js
+++ scripts/debugTools/utils.js
@@ -1,6 +1,7 @@
 export const showIndexOfPlace = (board) => {
   board.forEach((box, index) => {
     const el = document.createElement("p");
+    el.classList.add("notation", "debugLabel");
     el.textContent = index;
     box.onmouseover = () => {
       box.appendChild(el);
