beab846b0684d0a52546160d9ae95f797d23007d
5tupidmuffin <rameshwark9500@gmail.com>
2022-04-03 00:30:37 +0530

    removed redundant code

--- scripts/main.js
+++ scripts/main.js
@@ -44,52 +44,10 @@ const clickMove = (_, index) => {
       !ChessBoard.kills.includes(index)
     )
       return; // highlightedPlaces === LegalMoves
-    if (
-      // getPieceType(pastIndex).type === "king" &&
-      Math.abs(index - pastIndex) === 2
-    ) {
-      // its a castling move
-      let castlingData = highLightedPlaces.castling;
-      board[index] = board[pastIndex];
-      board[pastIndex] = 0;
-
-      if (castlingData?.[index]?.side === "left") {
-        // castling on the left side
-        ChessBoard.movePiece(castlingData?.[index]?.rook, index + 1);
-        // update board representation
-        board[index + 1] = board[castlingData?.[index]?.rook];
-        board[castlingData?.[index]?.rook] = 0;
-      } else {
-        // castling on the right side
-        ChessBoard.movePiece(castlingData?.[index]?.rook, index - 1);
-        // update board representation
-        board[index - 1] = board[castlingData?.[index]?.rook];
-        board[castlingData?.[index]?.rook] = 0;
-      }
-      ChessBoard.movePiece(pastIndex, index); // move the king
-      ChessBoard.removeAllHighlights();
-      piece_placed_sound.play();
-      return;
-    }
-
-    const enPassant = highLightedPlaces?.filter(
-      (move) => move?.enPassantKill && move.to === index
-    )[0];
-    if (enPassant) {
-      // its en passant
-      ChessBoard.movePiece(pastIndex, index);
-      ChessBoard.removePiece(enPassant.enPassantKill);
-      chess.doThisMove(enPassant);
-      ChessBoard.removeAllHighlights();
-      piece_placed_sound.play();
-      restrictions.enPassantKillable = null;
-      pastIndex = null;
-      return;
-    }
 
     ChessBoard.removeAllHighlights();
-    ChessBoard.movePiece(pastIndex, index);
-    const move = highLightedPlaces.filter((move) => move.to === index)[0];
+    const [move] = highLightedPlaces.filter((move) => move.to === index);
+    ChessBoard.doThisMove(move);
     chess.doThisMove(move);
     piece_placed_sound.play();
     pastIndex = null;
